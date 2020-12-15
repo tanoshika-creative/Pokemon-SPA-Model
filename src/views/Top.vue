7b6d5dccb27de7547b80037c5bc871c1e4cef66f
MaltesePoodle <creative.torvalds.monty@gmail.com>
2020-12-15 15:11:42 +0900

    fix: Top.vue

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="home">
-    <h1>これはなに?</h1>
-    <h3>ポケモンをつかまえてずかんをあつめるゲームです！</h3>
-    <h1>ポケモンをつかまえるには?</h1>
-    <h3>まずはいきたいばしょをえらびましょう！</h3>
+
+    <div class="box1">
+      <h1>ようこそ</h1>
+      <h3>ポケモンをつかまえてずかんをあつめるゲームです！</h3>
+    </div>
+
+    <div class="box2">
+      <h1>ポケモンをつかまえるには?</h1>
+      <h3>まずはいきたいばしょをえらびましょう！</h3>
+    </div>
+
     <h3>5つのエリアからえらべます！</h3>
-    <div><img :src="require(`@/assets/howTo/select_field.png`)" /></div>
+
+    <div>
+      <img class="description-screen-1" :src="require(`@/assets/howTo/select_field.png`)" />
+    </div>
     <h3>ポケモンたちがでてきたらつかまえたいポケモンをタッチ！</h3>
     <div><img :src="require(`@/assets/howTo/encount.png`)" /></div>
     <h3>ボールをなげてつかまえましょう！</h3>
     <div><img :src="require(`@/assets/howTo/get.png`)" /></div>
+    
     <h1>
       <router-link to="/adventure">いますぐポケモンをさがしにいく</router-link>
     </h1>
@@ -20,3 +31,23 @@
     </footer>
   </div>
 </template>
+
+<style scoped>
+  .box1 {
+    background-color: rgb(51, 125, 44);
+    color: #FFFFFF;
+    padding: 1rem 0;
+    margin: 0.4rem 0;
+  }
+  .box2 {
+    background-color: rgb(51, 125, 44);
+    color: #FFFFFF;
+    padding: 1rem 0;
+    margin: 0.4rem 0;
+  }
+
+  .description-screen-1 {
+    width: 75%;
+    height: auto;
+  }
+</style>
