e65f461f49b3fd066da7b5472bb0dcc1db2f3f40
MaltesePoodle <creative.torvalds.monty@gmail.com>
2020-12-15 14:07:05 +0900

    run: prettier for components

--- src/components/EncountPokemon.vue
+++ src/components/EncountPokemon.vue
@@ -1,81 +1,85 @@
 <template>
-	<div>
-	<div
-	id="box"
-	v-for="(pokemon, index) in selectEncountPokemons"
-	:pokemon="pokemon"
-	:key="index"
-	>
-    <img class="pokeImg" :src="require(`@/assets/sprites/${pokemon.pokemon_species.name}.png`)" @click="startButtle(pokemon.pokemon_species.name)">
-	</div>
-</div>
+  <div>
+    <div
+      id="box"
+      v-for="(pokemon, index) in selectEncountPokemons"
+      :pokemon="pokemon"
+      :key="index"
+    >
+      <img
+        class="pokeImg"
+        :src="require(`@/assets/sprites/${pokemon.pokemon_species.name}.png`)"
+        @click="startButtle(pokemon.pokemon_species.name)"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import anime from 'animejs'
+import anime from "animejs";
 export default {
-	props: {
-		pokemons: Array,
-	},
+  props: {
+    pokemons: Array,
+  },
   computed: {
-    selectEncountPokemons: function() {
+    selectEncountPokemons: function () {
       const totalRate = this.pokemons.reduce((previous, current) => {
-        return {rate:previous.rate + current.rate}
-      })
+        return { rate: previous.rate + current.rate };
+      });
       const encountPokemons = (pokemons) => {
-        const encountPokemons = []
+        const encountPokemons = [];
         // 出現数 11~3匹の範囲で出現
-        const appearanceNum = Math.floor(Math.random()*(11-3)+3);
+        const appearanceNum = Math.floor(Math.random() * (11 - 3) + 3);
         for (let i = appearanceNum; i > 0; i--) {
           const encount = Math.random() * totalRate.rate;
-          let searchPosition = 0.0
+          let searchPosition = 0.0;
           for (const pokemon of pokemons) {
-            searchPosition += pokemon.rate
+            searchPosition += pokemon.rate;
             if (encount < searchPosition) {
-              encountPokemons.push(pokemon)
-              break
+              encountPokemons.push(pokemon);
+              break;
             }
           }
         }
-        return encountPokemons
-      }
-      return encountPokemons(this.pokemons)
+        return encountPokemons;
+      };
+      return encountPokemons(this.pokemons);
     },
   },
   methods: {
     startButtle(name) {
-      this.$router.push({name: 'buttle', params: {name: name}})
+      this.$router.push({ name: "buttle", params: { name: name } });
     },
   },
   mounted() {
-    const x = document.getElementById('box').clientWidth / 4;
-    const y = document.getElementById('box').clientHeight * 2;
-    const pokeImgs = document.getElementsByClassName('pokeImg')
+    const x = document.getElementById("box").clientWidth / 4;
+    const y = document.getElementById("box").clientHeight * 2;
+    const pokeImgs = document.getElementsByClassName("pokeImg");
 
-    Object.keys(pokeImgs).forEach(function(key) {
-      const start_x = anime.random(-x, x)
-      const end_x = anime.random(-x, x)
-      const start_y = anime.random(-y, y)
-      const end_y = anime.random(-y, y)
-				anime({
-          targets: pokeImgs[key],
-          translateX: [start_x,end_x],
-          translateY: [start_y,end_y],
-          easing: 'linear',
-          loop: true,
-          direction: 'alternate',
-          duration: function() {
-            return anime.random(500,5000)
-          }
-        })
-      })
-  }
-}
+    Object.keys(pokeImgs).forEach(function (key) {
+      const start_x = anime.random(-x, x);
+      const end_x = anime.random(-x, x);
+      const start_y = anime.random(-y, y);
+      const end_y = anime.random(-y, y);
+      anime({
+        targets: pokeImgs[key],
+        translateX: [start_x, end_x],
+        translateY: [start_y, end_y],
+        easing: "linear",
+        loop: true,
+        direction: "alternate",
+        duration: function () {
+          return anime.random(500, 5000);
+        },
+      });
+    });
+  },
+};
 </script>
 <style>
-#box{
-	width: 50%;
-	height: 50%;
-	margin:0 auto
+#box {
+  width: 50%;
+  height: 50%;
+  margin: 0 auto;
 }
 </style>
